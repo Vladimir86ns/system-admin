5cb7ef8f9866a54430ee0626fd4859c624bb74e7
vladimir86ns <vladimir86ns@gmail.com>
2018-05-19 18:56:16 +0200

    feature - added relations and filliable fields

--- app/ProductCategory.php
+++ app/ProductCategory.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Project;
 
 class ProductCategory extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'project_id',
+    ];
+
+    protected $casts = [
+        'investment_collected' => 'boolean',
+    ];
+
+    /**
+     * The projects that belong to the product category.
+     */
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
