38a82a7fc2fefa668e410db926385312d7adbdf2
vladimir86ns <vladimir86ns@gmail.com>
2018-05-21 23:48:28 +0200

    add new migration investmnet

---
+++ app/Http/Controllers/InvestitionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Investition;
+use Illuminate\Http\Request;
+
+class InvestitionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Investition  $investition
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Investition $investition)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Investition  $investition
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Investition $investition)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Investition  $investition
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Investition $investition)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Investition  $investition
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Investition $investition)
+    {
+        //
+    }
+}
