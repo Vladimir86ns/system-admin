e280f821273eb9f98971be88d1f5145a78f23857
vladimir86ns <vladimir86ns@gmail.com>
2018-06-02 09:43:23 +0200

    bug/fix code style

--- app/Services/InvestmentsAdmin/InvestmentsAdminService.php
+++ app/Services/InvestmentsAdmin/InvestmentsAdminService.php
@@ -3,9 +3,77 @@
 namespace App\Services\InvestmentsAdmin;
 
 use App\InvestmentsAdmin;
+use App\Transformers\InvestmentsAdminTransformer;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Manager as FractalManager;
 
 class InvestmentsAdminService 
 {
+    /**
+     * @var FractalManager
+     */
+    protected $fractal;
+
+    /**
+     * @var InvestmentsAdminTransformer
+     */
+    protected $investmentsAdminTransformer;
+
+    /**
+     * InvestmentsAdminController
+     *
+     * @param FractalManager $fractal
+     */
+    public function __construct(
+        FractalManager $fractalManager,
+        InvestmentsAdminTransformer $investmentsAdminTransformer
+    ) {
+        $this->fractal = $fractalManager;
+        $this->investmentsAdminTransformer = $investmentsAdminTransformer;
+    }
+
+    /**
+     * Get all investments
+     *
+     * @param FractalManager $fractal
+     */
+    public function getAllInvestments()
+    {
+        return InvestmentsAdmin::get();
+    }
+
+    /**
+     * Get all investments from transformer
+     * 
+     * @param array $attributes
+     */
+    public function getAllInvestmentsFromTransformer()
+    {
+        $result = new Collection($this->getAllInvestments(), $this->investmentsAdminTransformer);
+
+        return $this->fractal->createData($result)->toArray();
+    }
+
+    /**
+     * Get single investment
+     *
+     * @param FractalManager $fractal
+     */
+    public function getInvestment($id)
+    {
+        return InvestmentsAdmin::find($id);
+    }
+
+    /**
+     * Get single investment
+     *
+     * @param FractalManager $fractal
+     */
+    public function getInvestmentFromTransformer($id)
+    {
+        return $this->investmentsAdminTransformer->transform($this->getInvestment($id));
+    }
+
     /**
      * Store new investments in DB
      * 
