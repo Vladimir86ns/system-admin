f70b417eb2f8c52e61de4b83f4086befb7d24042
vladimir86ns <vladimir86ns@gmail.com>
2018-07-10 20:04:43 +0200

    fix review

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,12 +6,21 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    /**
+     * Show all products for company.
+     *
+     * @return View
+     */
     public function index()
     {
-        // dd('index');
         return view('owner.product.index');
     }
 
+    /**
+     * Add new project to company.
+     *
+     * @return View
+     */
     public function create()
     {
         return view('owner.product.form.add-product');
