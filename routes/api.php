4acd9197abc67f992be3cb1cdafe58734dc7b3c9
vladimir86ns <vladimir86ns@gmail.com>
2018-07-19 20:51:56 +0200

    feature/added-corse

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group([ 'prefix' => 'investment'], function () {
-    Route::get('signin', 'InvestmentController@getSignin')->name('investment-signin');
-    Route::get('login', 'InvestmentController@getSignin')->name('investment-login');
-    Route::post('investment-signup', 'InvestmentController@postSignup')->name('investment-signup');
-
-        # Dashboard / Index
-        Route::get('/', 'InvestmentController@showHome')->name('investor-dashboard');
+Route::group([ 'prefix' => 'company'], function () {
+    Route::group([ 'prefix' => '/{id}/product'], function () {
+        Route::get('/get_all', 'ApiProductController@getAll');
+    });
 });
